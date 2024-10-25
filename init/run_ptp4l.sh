1aa09c97dbf856c36fc7c1c433f140587ede21ae
James Burgoyne <thejaburjunk+github@gmail.com>
2024-10-25 09:29:37 -0700

    debug

--- init/run_ptp4l.sh
+++ init/run_ptp4l.sh
@@ -2,8 +2,12 @@
 interface=$1
 mac_address=$2
 
+# Determine the directory where this script is located
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Kill existing ptp4l processes
 killall ptp4l
 
 # Run ptp4l with the specified interface and MAC address
-ptp4l -i $interface -f gPTP.cfg --step_threshold=1 -p $mac_address &
+#ptp4l -i $interface -f gPTP.cfg --step_threshold=1 -p $mac_address &
+ptp4l -i "$interface" -f "$script_dir/gPTP.cfg" --step_threshold=1 -p "$mac_address" &
