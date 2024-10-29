4a01baa18cd80b9e72e2e86f634b3081121f571f
James Burgoyne <thejaburjunk+github@gmail.com>
2024-10-29 08:18:48 -0700

    debug

--- scripts/start_drone.sh
+++ scripts/start_drone.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-# set -x
 
-# Store the drone number from the argument
 DRONE_NUMBER=$1
 
-# Become root and run commands in bash explicitly
+# Become root
 sudo bash <<EOF
-# Source the environment
-source /home/xilinx/xilinx/activate
 
-# Change to the directory containing drone.py
+# Source the environment
+source activate
+
+# Change to the directory containing drone.py for PATH issues
 cd /home/xilinx/primecam_readout/src
 
-# Add the directory containing _cfg_drone1 to the PYTHONPATH if needed
-#export PYTHONPATH=\$PYTHONPATH:/home/xilinx/primecam_readout/src
-
-# Run the drone.py script with the drone number as an argument
+# Run the drone.py script
 exec /usr/local/share/pynq-venv/bin/python3 /home/xilinx/primecam_readout/src/drone.py $DRONE_NUMBER
 EOF
